ef31abb456621bcaff7bd3a6b7db7d43c3352078
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-20 23:29:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,6 +76,10 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  consolidate_cart = consolidated_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  clearanced_cart = apply_clearance(couponed_cart)
+  
   
   
   
